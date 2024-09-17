422da8f0f2705479906e94803644ac4ebeac4312
emilylienhuang <eng@uccs.edu>
2024-09-17 16:14:41 -0600

    lousy algorithm

--- lexicon.py
+++ lexicon.py
@@ -1,6 +1,12 @@
-import spacy
+import collections
+import heapq
+import random
 
-import spacy_ngram
+import spacy
+import nltk
+from nltk import ngrams
+from collections import defaultdict
+
 import re
 
 #load spacy english model
@@ -24,15 +30,83 @@ def genLexicon(file_path, lang):
     #process the cleaned text through nlp pipline using spacy
     doc = nlp(cleaned_txt)
 
-    lexicon = set()
+    lexicon = []
+    #do I want have repeated values in my lexicon
 
     #extract tokens to create the lexicon
     for token in doc:
         # remove punctuations, excess white spaces, words
         if not token.is_punct and not token.is_space and not token.is_stop and not token.is_digit:
-            # chop off and only add root words or meanings into the vocabulary to dumb down to
-            #simplest parts
-            if token.lemma_ not in lexicon:
-                lexicon.add(token.lemma_.lower())
+            lexicon.append(token.lower_)
+
     return lexicon
-lexicon = genLexicon('./English/English1k.txt', 'en_core_web_sm')
+
+#wrapper to create ngrams using the nltk library
+#text is a list of words
+# num is the n in ngram
+# outputs a generator that can be indexed
+def genNGrams(text, num):
+    return list(ngrams(text, num))
+
+def calculateProbabilities(grams):
+    gram_to_prob = {}
+    for g in grams:
+        gram_to_prob[g] = 1 + gram_to_prob.get(g,0)
+    for gram, prob in gram_to_prob.items():
+        gram_to_prob[gram] /= len(gram_to_prob.values())
+    return gram_to_prob
+
+def loadHeap(word, probs):
+    heap = []
+    for g,p in probs.items():
+        if g[0] == word:
+            heapq.heappush(heap, (-p, g[1]))
+    return heap
+'''
+logic: I choose one of the most likely occurring choices
+if no choice availbale, I choose a random word to get the ball rolling again
+I'm making nonsense... Make it make more sense with 3-grams?
+'''
+def writePoem(file_path, lang, stanzas, line_length):
+    # create the body of text
+    text = genLexicon(file_path, lang)
+
+    #calculate the grams
+    grams = genNGrams(text, 2)
+
+    # assign probabilities
+    probs = calculateProbabilities(grams)
+
+
+    poem = []
+    for _ in range(3):
+        for _ in range(stanzas):
+            phrase = []
+            heap = []
+            for _ in range(line_length):
+                if phrase:
+                    heap = loadHeap(phrase[-1], probs)
+                if not phrase or not heap:
+                    # if there is not a corresponding ngram or we are at the start of a new line
+                    for _ in range(20):
+                        heapq.heappush(heap, (-random.randint(0, 100)/100.0, random.choice(text)))
+                #alternate between least and most occurring
+                for _ in range(random.randint(1,3)):
+                    if heap:
+                        _, new_word = heapq.heappop(heap)
+                    else:
+                        break
+                phrase.append(new_word)
+                heap = []
+            string = ' '.join(phrase[:-1]) + ' ' + phrase[-1] + ","
+            poem.append(string)
+        poem.append('')
+    return poem
+
+def formatPoem(poem, stanza):
+        for line in poem:
+            print(line)
+formatPoem(writePoem('./English/English200.txt', 'en_core_web_sm',  4, 4), 4)
+#
+
+
